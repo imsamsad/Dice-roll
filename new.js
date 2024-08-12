f5e97a72e8974552cf4765b5df60aee1ed326471
imsamsad <154818382+imsamsad@users.noreply.github.com>
2024-08-12 21:58:36 +0530

    Update new.js

--- new.js
+++ new.js
@@ -7,12 +7,12 @@ function play(){
     setTimeout(() => { console.log('World!'); }, 2000);
         var random=Math.random()*6+1;
     random=Math.floor(random);
-    var source="./dice"+random+".png";
+    var source="dice"+random+".png";
     document.querySelectorAll("img")[0].setAttribute("src",source);
     
     var random2=Math.random()*6+1;
     random2=Math.floor(random2);
-    var source2="./dice"+random2+".png";
+    var source2="dice"+random2+".png";
     document.querySelectorAll("img")[1].setAttribute("src",source2);
 
  
